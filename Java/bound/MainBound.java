2077529a5834e292ed7b183e704f888eb3002c0f
Himanshi Joshi <joshihimanshi475@gmail.com>
2026-03-31 21:11:00 +0530

    Create MainBound.java

---
+++ Java/bound/MainBound.java
@@ -0,0 +1,16 @@
+package bound;
+
+import java.beans.*;
+
+public class MainBound {
+    public static void main(String[] args) {
+
+        BoundPerson p = new BoundPerson();
+
+        p.addPropertyChangeListener(evt -> {
+            System.out.println("Changed: " + evt.getPropertyName());
+        });
+
+        p.setName("Himanshi");
+    }
+}
